0f0b9ddfd6c24953875643c169e2dc41a375cd24
CoderArsh <arshjeetb08@gmail.com>
2021-12-01 13:42:56 +0530

    Added: To check email is valid of user with the help regex patterns

--- js/simple_form.js
+++ js/simple_form.js
@@ -10,11 +10,24 @@ salary.addEventListener('input', function(){
 // UC1 - Valid First Name 
 
 const text = document.querySelector('#text');
-            const textError = document.querySelector('.text-error');
-            text.addEventListener('input', function(){
-                let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-                if (nameRegex.test(text.value))
-                textError.textContent = "";
-                else textError.textContent = "Name is Incorrect";
-                
-            });
+const textError = document.querySelector('.text-error');
+    text.addEventListener('input', function(){
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if (nameRegex.test(text.value)){
+        textError.textContent = "";
+    }
+    else textError.textContent = "Name is Incorrect";
+    });
+
+// UC2 - Valid Email 
+
+const email = document.querySelector('#emailInput');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function () {
+    let emailRegex = RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
+    if (emailRegex.test(email.value)) {
+      emailError.textContent = "";
+    } else 
+      emailError.textContent = "Invalid Email";
+    });
+
